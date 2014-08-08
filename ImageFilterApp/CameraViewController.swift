f7f66d735a10f1dbbf9bb53d7bc8492e01789e1d
Jeff Gayle <jeff.j.gayle@gmail.com>
2014-08-08 16:49:13 -0700

    First attempt at AV Foundation

---
+++ ImageFilterApp/CameraViewController.swift
@@ -0,0 +1,13 @@
+//
+//  CameraViewController.swift
+//  ImageFilterApp
+//
+//  Created by Jeff Gayle on 8/8/14.
+//  Copyright (c) 2014 Jeff Gayle. All rights reserved.
+//
+
+import Cocoa
+
+class CameraViewController: UIViewController {
+
+}
